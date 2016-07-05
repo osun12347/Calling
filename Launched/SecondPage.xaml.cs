f89ae842bd76ff67c5bc54bbb438935a79706f15
Neal <Neal@V-DOXIE-W10>
2016-07-05 17:43:53 +0800

    Add project files.

---
+++ Launched/SecondPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Launched
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SecondPage : Page
+    {
+        private Windows.System.ProtocolForResultsOperation _operation = null;
+
+        public SecondPage()
+        {
+            this.InitializeComponent();
+
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var protocolForResultsArgs = e.Parameter as ProtocolForResultsActivatedEventArgs;
+            // Set the ProtocolForResultsOperation field.
+            _operation = protocolForResultsArgs.ProtocolForResultsOperation;
+
+            if (protocolForResultsArgs.Data.ContainsKey("TestData"))
+            {
+                string dataFromCaller = protocolForResultsArgs.Data["TestData"] as string;
+            }
+            ValueSet result = new ValueSet();
+            result["ReturnedData"] = "The returned result";
+            _operation.ReportCompleted(result);
+
+        }
+    }
+}
